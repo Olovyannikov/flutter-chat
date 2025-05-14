38b28ba1fe0764ae5fa5d2a1ef33bd5c1daf10f8
Ilya Olovyannikov <excaliburgyro@gmail.com>
2025-05-14 14:21:54 +0300

    feat(sentry): add sentry

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'package:flutter_chat/entities/User/user_entity.dart';
 import 'package:flutter_chat/pages/pages.dart';
 
-void main()async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const MyApp());
+Future<void> main() async {
+  await SentryFlutter.init(
+    (options) {
+      options.dsn =
+          'https://fa391306666b0286b064cf8f7b64e87f@o4509321161867266.ingest.de.sentry.io/4509321162981456';
+      options.sendDefaultPii = true;
+    },
+    appRunner: () async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      runApp(SentryWidget(child: MyApp()));
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -16,11 +27,11 @@ class MyApp extends StatelessWidget {
 
   Widget _buildHomeScreen() {
     return StreamBuilder<User?>(
-        stream: FirebaseAuth.instance.authStateChanges(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) return PostsScreen();
-          return SignInScreen();
-        }
+      stream: FirebaseAuth.instance.authStateChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) return PostsScreen();
+        return SignInScreen();
+      },
     );
   }
 
@@ -36,10 +47,9 @@ class MyApp extends StatelessWidget {
           SignUpScreen.id: (context) => SignUpScreen(),
           PostsScreen.id: (context) => PostsScreen(),
           CreatePostScreen.id: (context) => CreatePostScreen(),
-          ChatScreen.id: (context) => ChatScreen()
+          ChatScreen.id: (context) => ChatScreen(),
         },
       ),
     );
   }
 }
-
